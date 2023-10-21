a73595c685f924ffafe56ba204e78f11052d29a9
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-21 01:52:22 +0100

    Update AnytownTest.java

--- AnytownTest.java
+++ AnytownTest.java
@@ -1,3 +1,4 @@
+```java
 package Anytown;
 
 import java.util.Scanner;
@@ -6,63 +7,68 @@ import java.io.File;
 public class AnytownTest {
     public static void main(String[] args) throws Exception {
 
-        ArrayBag buildings = new ArrayBag(13);
-        File fileIn = new File("Anytown/buildings.txt");
-        Scanner fileInput = new Scanner(fileIn);
-        String buildingData;
-        int marker1 = 2, marker2, marker3, marker4, marker5;                     
-        String address, owner;
-        int numberOfBedrooms, numberOfEmplyees, avgTurnover;
-        boolean hasGarage;
-        
+        ArrayBag buildings = new ArrayBag(13);  // 创建一个容量为13的ArrayBag对象，用于存储建筑物
+        File fileIn = new File("Anytown/buildings.txt");  // 创建一个File对象，指向建筑物数据文件
+        Scanner fileInput = new Scanner(fileIn);  // 创建一个Scanner对象，用于读取文件中的数据
+        String buildingData;  // 存储从文件中读取的建筑物数据
+        int marker1 = 2, marker2, marker3, marker4, marker5;  // 标记建筑物数据中各个字段的位置                     
+        String address, owner;  // 建筑物的地址和所有者
+        int numberOfBedrooms, numberOfEmplyees, avgTurnover;  // 房屋的卧室数量、商店的员工数量和平均营业额
+        boolean hasGarage;  // 房屋是否有车库
 
+        // 逐行读取建筑物数据
         while (fileInput.hasNextLine()) {
             buildingData = fileInput.nextLine();
+
+            // 通过索引位置找到建筑物数据中各个字段的位置
             marker2 = buildingData.indexOf("|", marker1);
             marker3 = buildingData.indexOf("|", marker2 + 1);
             marker4 = buildingData.indexOf("|", marker3 + 1);
             marker5 = buildingData.indexOf("|", marker4 + 1);
 
+            // 从建筑物数据中提取地址和所有者
             address = buildingData.substring(marker1, marker2);
             owner = buildingData.substring(marker2 + 1, marker3);
 
-             switch (buildingData.substring(0,1)) {
+            // 根据建筑物类型进行处理
+            switch (buildingData.substring(0,1)) {
                 case "B" :
-                System.out.println("Building");
+                System.out.println("Building");  // 打印类型为建筑物
                 if (buildings.addNewEntry(new Building(address, owner))) {
-                    System.out.println("Building added");
+                    System.out.println("Building added");  // 添加建筑物成功
                 }
                 else {
-                    System.out.println("Building not added");
+                    System.out.println("Building not added");  // 添加建筑物失败
                 }
                 break;
 
                 case "H" :
-                System.out.println("House");
+                System.out.println("House");  // 打印类型为房屋
                 numberOfBedrooms = Integer.parseInt(buildingData.substring(marker3 + 1, marker4));
                 hasGarage = Boolean.parseBoolean(buildingData.substring(marker4 + 1, marker5));
                 if (buildings.addNewEntry(new House(address, owner, numberOfBedrooms, hasGarage))) {
-                    System.out.println("House added");
+                    System.out.println("House added");  // 添加房屋成功
                 }
                 else {
-                    System.out.println("House not added");
+                    System.out.println("House not added");  // 添加房屋失败
                 }
                 break;
 
                 case "S" :
-                System.out.println("Shop");
+                System.out.println("Shop");  // 打印类型为商店
                 numberOfEmplyees = Integer.parseInt(buildingData.substring(marker3 + 1, marker4));
                 avgTurnover = Integer.parseInt(buildingData.substring(marker4 + 1, marker5));
                 if (buildings.addNewEntry(new Shop(address, owner, numberOfEmplyees, avgTurnover))) {
-                    System.out.println("Shop added");
+                    System.out.println("Shop added");  // 添加商店成功
                 }
                 else {
-                    System.out.println("Shop not added");
+                    System.out.println("Shop not added");  // 添加商店失败
                 }
                 break;
             }
         }
 
-       fileInput.close();
+       fileInput.close();  // 关闭文件读取
     }
 }
+```
