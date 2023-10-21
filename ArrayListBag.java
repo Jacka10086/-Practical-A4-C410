168b89390d7ad50aba5e9ff90d7248e6083facd6
Jacka10086 <111729221+Jacka10086@users.noreply.github.com>
2023-10-21 01:55:35 +0100

    Update ArrayListBag.java

--- ArrayListBag.java
+++ ArrayListBag.java
@@ -5,57 +5,53 @@ import java.util.Collections;
 
 public class ArrayListBag implements BagInterface {
 
-    private ArrayList<Building> buildings = new ArrayList<Building>();  
+    private ArrayList<Building> buildings = new ArrayList<Building>();  // 创建一个ArrayList，用于存储Building对象
 
     public int getCurrentSize() {
-        return this.buildings.size();
+        return this.buildings.size();  // 返回ArrayList中元素的个数，即当前大小
     }
 
     public boolean isEmpty() {
-        return this.buildings.size() == 0;
+        return this.buildings.size() == 0;  // 判断ArrayList是否为空
     }
 
     public boolean addNewEntry(Building newEntry) {
-        this.buildings.add(newEntry);
+        this.buildings.add(newEntry);  // 向ArrayList中添加新的Building对象
         return true;    
     }
 
     public Building remove() {
         if(!isEmpty()) {
-            Building thisBuilding = this.buildings.get(0);
-            this.buildings.remove(0);
-            return thisBuilding;
+            Building thisBuilding = this.buildings.get(0);  // 获取ArrayList中索引为0的Building对象
+            this.buildings.remove(0);  // 移除ArrayList中索引为0的Building对象
+            return thisBuilding;  // 返回被移除的Building对象
         } else return null;
     }
 
     public boolean remove(Building anEntry) {
-        return this.buildings.remove(anEntry);
+        return this.buildings.remove(anEntry);  // 移除ArrayList中指定的Building对象
     }
 
     public void clear()  {
-        this.buildings.clear();
+        this.buildings.clear();  // 清空ArrayList中的所有元素
 
     }
 
     public int getFrequencyOf(Building anEntry) {
-       return Collections.frequency(this.buildings, anEntry);
-            }
+       return Collections.frequency(this.buildings, anEntry);  // 返回ArrayList中指定Building对象的出现次数
+    }
 
 
     public boolean contains(Building anEntry) {
-        return this.buildings.contains(anEntry);
+        return this.buildings.contains(anEntry);  // 判断ArrayList中是否包含指定的Building对象
     }
 
     public String toString() {
         String resultStr = "Bag [ \n";
         for (Building building : buildings) {
-            resultStr += building.toString() + "\n";
+            resultStr += building.toString() + "\n";  // 将ArrayList中的每个Building对象转为字符串，并添加到resultStr中
         }
         resultStr += "] \n";
         return resultStr;
-    }
-
-    
-
-     
+    }    
 }
